bdc22975d6949b2f398709d77113652b5df93673
정유진 개발5팀 <eg.jeong@theenm.com>
2020-03-07 23:57:35 +0900

    샘플 테스트 셋업

---
+++ fastlaneSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastlaneSample
+//
+//  Created by Eugene Jeong on 2020/03/04.
+//  Copyright © 2020 Eugene Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
